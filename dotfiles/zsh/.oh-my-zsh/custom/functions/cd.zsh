1a0af507819e6520215ea729a375e2623e18b6aa
Marcin Stelmaszyk <mstelmaszyk@proton.me>
2026-08-13 09:24:03 +0200

    Fix argument handling in ll and la functions
    Updated ll and la functions to correctly handle arguments.

--- dotfiles/zsh/.oh-my-zsh/custom/functions/cd.zsh
+++ dotfiles/zsh/.oh-my-zsh/custom/functions/cd.zsh
@@ -1,11 +1,11 @@
 unalias ll 2>/dev/null
 function ll {
-    \ls -lh --group-directories-first --time-style=long-iso --color=auto "@"
+    \ls -lh --group-directories-first --time-style=long-iso --color=auto "$@"
 }
 
 unalias la 2>/dev/null
 function la {
-    \ls -Alh --group-directories-first --time-style=long-iso --color=auto "@"
+    \ls -Alh --group-directories-first --time-style=long-iso --color=auto "$@"
 }
 
 function cd {
